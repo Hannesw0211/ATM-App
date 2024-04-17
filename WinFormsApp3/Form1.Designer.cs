b011992a8ce2bc2116b341aa09248a89ed2658fc
Hannes <hannes.w211@gmail.com>
2024-04-17 18:38:25 +0200

    Login System

--- WinFormsApp3/Form1.Designer.cs
+++ WinFormsApp3/Form1.Designer.cs
@@ -28,45 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
+            label_noticeLogin = new Label();
+            textBox1_CCdetails = new TextBox();
             textBox1 = new TextBox();
-            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
+            // 
+            // label_noticeLogin
+            // 
+            label_noticeLogin.AutoSize = true;
+            label_noticeLogin.Location = new Point(97, 51);
+            label_noticeLogin.Name = "label_noticeLogin";
+            label_noticeLogin.Size = new Size(206, 15);
+            label_noticeLogin.TabIndex = 0;
+            label_noticeLogin.Text = "Please enter your Card details to login";
+            // 
+            // textBox1_CCdetails
+            // 
+            textBox1_CCdetails.Location = new Point(97, 79);
+            textBox1_CCdetails.Name = "textBox1_CCdetails";
+            textBox1_CCdetails.Size = new Size(179, 23);
+            textBox1_CCdetails.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(97, 108);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(178, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(281, 108);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 4;
+            button2.Text = "Login";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(78, 26);
+            label1.Location = new Point(12, 82);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 0;
-            label1.Text = "label1";
+            label1.Size = new Size(79, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Card Number";
             // 
-            // textBox1
+            // label2
             // 
-            textBox1.Location = new Point(107, 123);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 1;
+            label2.AutoSize = true;
+            label2.Location = new Point(62, 112);
+            label2.Name = "label2";
+            label2.Size = new Size(29, 15);
+            label2.TabIndex = 6;
+            label2.Text = "PIN:";
             // 
-            // button1
+            // label3
             // 
-            button1.Location = new Point(276, 102);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 2;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            label3.AutoSize = true;
+            label3.Location = new Point(100, 148);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 15);
+            label3.TabIndex = 7;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button1);
-            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(textBox1_CCdetails);
+            Controls.Add(label_noticeLogin);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -75,8 +116,12 @@
 
         #endregion
 
-        private Label label1;
+        private Label label_noticeLogin;
+        private TextBox textBox1_CCdetails;
         private TextBox textBox1;
-        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- WinFormsApp3/Form1.cs
+++ WinFormsApp3/Form1.cs
@@ -8,29 +8,38 @@ namespace WinFormsApp3
         {
             InitializeComponent();
         }
-
-        string[] kunden = ["5050", "6060", "7070"];
+        //CC Number , PIN 
+        //TODO: Database
+        string[,] kunden = { { "1010202030304040", "1111" }, { "2020303040405050", "2222" }, { "3030404050506060", "3333" }, { "4040505060607070", "4444" }, { "5050606070708080", "5555" }, { "6060707080809090", "6666" } };
         bool UserFound;
-        private void button1_Click(object sender, EventArgs e)
-        {
-            String CCdetails = textBox1.Text;
+        int kundenID;
 
-            for (int i = 0; i < kunden.Length; i++)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String CCdetails = textBox1_CCdetails.Text;
+            String PIN = textBox1.Text;
+
+            for (int i = 0; i < (kunden.Length / 2); i++)
             {
-                
-                
-                if (CCdetails == kunden[i])
+
+
+                if (CCdetails == kunden[i, 0])
                 {
                     UserFound = true;
+                    kundenID = i;
                 }
                 else
                 {
                     continue;
                 }
             }
-            if (UserFound)
+            if (UserFound && PIN == kunden[kundenID,1])
             {
-                label1.Text = "Success";
+                label3.Text = "Success";                
+            }
+            else
+            {
+                label3.Text = "Etwas ist Schiefgelaufen";
             }
         }
     }
