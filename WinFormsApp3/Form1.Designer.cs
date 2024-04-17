de43260b6122d34c478b037bc1eb42b447a5616c
Hannes <hannes.w211@gmail.com>
2024-04-17 21:12:24 +0200

    Bank balance und withdraws

--- WinFormsApp3/Form1.Designer.cs
+++ WinFormsApp3/Form1.Designer.cs
@@ -35,12 +35,19 @@
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
+            panel1 = new Panel();
+            tb_withdraw_amount = new TextBox();
+            bt_withdraw = new Button();
+            label_notice_withdraw = new Label();
+            label_cash_available = new Label();
+            label_notice_moneyavailable = new Label();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // label_noticeLogin
             // 
             label_noticeLogin.AutoSize = true;
-            label_noticeLogin.Location = new Point(97, 51);
+            label_noticeLogin.Location = new Point(118, 134);
             label_noticeLogin.Name = "label_noticeLogin";
             label_noticeLogin.Size = new Size(206, 15);
             label_noticeLogin.TabIndex = 0;
@@ -48,21 +55,21 @@
             // 
             // textBox1_CCdetails
             // 
-            textBox1_CCdetails.Location = new Point(97, 79);
+            textBox1_CCdetails.Location = new Point(118, 162);
             textBox1_CCdetails.Name = "textBox1_CCdetails";
             textBox1_CCdetails.Size = new Size(179, 23);
             textBox1_CCdetails.TabIndex = 1;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(97, 108);
+            textBox1.Location = new Point(118, 191);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(178, 23);
             textBox1.TabIndex = 3;
             // 
             // button2
             // 
-            button2.Location = new Point(281, 108);
+            button2.Location = new Point(302, 191);
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
             button2.TabIndex = 4;
@@ -73,7 +80,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 82);
+            label1.Location = new Point(33, 165);
             label1.Name = "label1";
             label1.Size = new Size(79, 15);
             label1.TabIndex = 5;
@@ -82,7 +89,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(62, 112);
+            label2.Location = new Point(83, 195);
             label2.Name = "label2";
             label2.Size = new Size(29, 15);
             label2.TabIndex = 6;
@@ -91,16 +98,79 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(100, 148);
+            label3.Location = new Point(121, 231);
             label3.Name = "label3";
             label3.Size = new Size(0, 15);
             label3.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(tb_withdraw_amount);
+            panel1.Controls.Add(bt_withdraw);
+            panel1.Controls.Add(label_notice_withdraw);
+            panel1.Controls.Add(label_cash_available);
+            panel1.Controls.Add(label_notice_moneyavailable);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(418, 365);
+            panel1.TabIndex = 8;
+            panel1.Visible = false;
+            // 
+            // tb_withdraw_amount
+            // 
+            tb_withdraw_amount.Location = new Point(221, 70);
+            tb_withdraw_amount.Name = "tb_withdraw_amount";
+            tb_withdraw_amount.Size = new Size(100, 23);
+            tb_withdraw_amount.TabIndex = 4;
+            tb_withdraw_amount.Visible = false;
+            // 
+            // bt_withdraw
+            // 
+            bt_withdraw.Location = new Point(327, 69);
+            bt_withdraw.Name = "bt_withdraw";
+            bt_withdraw.Size = new Size(75, 23);
+            bt_withdraw.TabIndex = 3;
+            bt_withdraw.Text = "Withdraw";
+            bt_withdraw.UseVisualStyleBackColor = true;
+            bt_withdraw.Visible = false;
+            bt_withdraw.Click += bt_withdraw_Click;
+            // 
+            // label_notice_withdraw
+            // 
+            label_notice_withdraw.AutoSize = true;
+            label_notice_withdraw.Location = new Point(221, 42);
+            label_notice_withdraw.Name = "label_notice_withdraw";
+            label_notice_withdraw.Size = new Size(165, 15);
+            label_notice_withdraw.TabIndex = 2;
+            label_notice_withdraw.Text = "Enter the amount to withdraw";
+            label_notice_withdraw.Visible = false;
+            // 
+            // label_cash_available
+            // 
+            label_cash_available.AutoSize = true;
+            label_cash_available.Location = new Point(44, 73);
+            label_cash_available.Name = "label_cash_available";
+            label_cash_available.Size = new Size(69, 15);
+            label_cash_available.TabIndex = 1;
+            label_cash_available.Text = "placeholder";
+            label_cash_available.Visible = false;
+            // 
+            // label_notice_moneyavailable
+            // 
+            label_notice_moneyavailable.AutoSize = true;
+            label_notice_moneyavailable.Location = new Point(44, 42);
+            label_notice_moneyavailable.Name = "label_notice_moneyavailable";
+            label_notice_moneyavailable.Size = new Size(93, 15);
+            label_notice_moneyavailable.TabIndex = 0;
+            label_notice_moneyavailable.Text = "Money available";
+            label_notice_moneyavailable.Visible = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(442, 389);
+            Controls.Add(panel1);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -110,6 +180,8 @@
             Controls.Add(label_noticeLogin);
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -123,5 +195,11 @@
         private Label label1;
         private Label label2;
         private Label label3;
+        private Panel panel1;
+        private TextBox tb_withdraw_amount;
+        private Button bt_withdraw;
+        private Label label_notice_withdraw;
+        private Label label_cash_available;
+        private Label label_notice_moneyavailable;
     }
 }
--- WinFormsApp3/Form1.cs
+++ WinFormsApp3/Form1.cs
@@ -10,7 +10,7 @@ namespace WinFormsApp3
         }
         //CC Number , PIN 
         //TODO: Database
-        string[,] kunden = { { "1010202030304040", "1111" }, { "2020303040405050", "2222" }, { "3030404050506060", "3333" }, { "4040505060607070", "4444" }, { "5050606070708080", "5555" }, { "6060707080809090", "6666" } };
+        string[,] kunden = { { "1010", "1111", "1405,64" }, { "2020303040405050", "2222", "10,34" }, { "3030404050506060", "3333", "502,13" }, { "4040505060607070", "4444", "1,34" }, { "5050606070708080", "5555", "10404,41" }, { "6060707080809090", "6666", "692,29" } };
         bool UserFound;
         int kundenID;
 
@@ -19,7 +19,7 @@ namespace WinFormsApp3
             String CCdetails = textBox1_CCdetails.Text;
             String PIN = textBox1.Text;
 
-            for (int i = 0; i < (kunden.Length / 2); i++)
+            for (int i = 0; i < (kunden.Length / 3); i++)
             {
 
 
@@ -33,14 +33,37 @@ namespace WinFormsApp3
                     continue;
                 }
             }
-            if (UserFound && PIN == kunden[kundenID,1])
+            if (UserFound && PIN == kunden[kundenID, 1])
             {
-                label3.Text = "Success";                
+                label3.Text = "Success";
+                login_success();
             }
             else
             {
                 label3.Text = "Etwas ist Schiefgelaufen";
+
             }
+        }
+
+        private void login_success()
+        {
+            panel1.Visible = true;
+            label_cash_available.Visible = true;
+            label_cash_available.Text = kunden[kundenID, 2];
+            label_notice_moneyavailable.Visible = true;
+            label_notice_withdraw.Visible = true;
+            tb_withdraw_amount.Visible = true;
+            bt_withdraw.Visible = true;
+
+        }
+
+        private void bt_withdraw_Click(object sender, EventArgs e)
+        {
+            String amountWithdraw = tb_withdraw_amount.Text;
+            double cash_available = double.Parse(kunden[kundenID,2]);
+            double cash_after_withdraw = cash_available - double.Parse(amountWithdraw); 
+            cash_after_withdraw = Math.Round(cash_after_withdraw, 2);
+            label_cash_available.Text = cash_after_withdraw.ToString();
         }
     }
 }
